bbd123e9cea19f2624ddef14704bcc185469724d
Ali Awwad <ali.awwad@adnec.ae>
2025-05-07 23:15:34 +0400

    add bot to git

--- config/statamic/git.php
+++ config/statamic/git.php
@@ -78,8 +78,8 @@ return [
     'use_authenticated' => true,
 
     'user' => [
-        'name' => env('STATAMIC_GIT_USER_NAME', 'Spock'),
-        'email' => env('STATAMIC_GIT_USER_EMAIL', 'spock@example.com'),
+        'name' => env('STATAMIC_GIT_USER_NAME', 'WebAdmin'),
+        'email' => env('STATAMIC_GIT_USER_EMAIL', 'WebAdmin@alainihex'),
     ],
 
     /*
@@ -134,7 +134,7 @@ return [
 
     'commands' => [
         '{{ git }} add {{ paths }}',
-        '{{ git }} -c "user.name={{ name }}" -c "user.email={{ email }}" commit -m "{{ message }}"',
+        '{{ git }} -c "user.name={{ name }}" -c "user.email={{ email }}" commit -m "{{ message }} [BOT]"',
     ],
 
     /*
