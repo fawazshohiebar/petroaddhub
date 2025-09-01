9f82a70fa643caf1d9b3adfc58317f4486b373b8
Ali Awwad <ali.awwad@adnec.ae>
2025-09-01 13:53:47 +0400

    make the commit every minute

--- routes/console.php
+++ routes/console.php
@@ -8,4 +8,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('statamic:git:commit')->everyTenMinutes();
+Schedule::command('statamic:git:commit')->everyMinute();
